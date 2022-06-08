fef8e69cc6f30266d268806d5a9ddcb6126da296
pviscone <pieroviscone@gmail.com>
2022-06-08 19:11:11 +0200

    Prova fallimento

--- test/Other/OtherTest.cpp
+++ test/Other/OtherTest.cpp
@@ -3,4 +3,8 @@ TEST_CASE("Prova a caso"){
     SECTION("Prova a caso 1"){
         REQUIRE(1==1);
     };
+    TEST_CASE("Prova a caso"){
+    SECTION("Prova a caso fallimento"){
+        REQUIRE(1==0);
+    };
 };
