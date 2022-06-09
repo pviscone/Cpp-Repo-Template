21d26c05bf3a0c2655193e73fb822046a116845f
pviscone <pieroviscone@gmail.com>
2022-06-09 02:10:14 +0200

    Modified doxygen action

--- docs/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['data_0',['data',['../class_sample.html#a0e730e51ffde4df94e2fd64956379f42',1,'Sample']]]
-];
--- docs/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['getdata_7',['GetData',['../class_sample.html#aa93150b058634e944b432e117daf5701',1,'Sample']]]
-];
